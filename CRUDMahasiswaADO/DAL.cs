c3c7b7fd36fa68985950e7b53538ee2b3442d474
ramdaniasyafitriani <ramdaniania04@gmail.com>
2026-06-22 09:31:02 +0700

    membuat koneksi untuk DAL.cs

---
+++ CRUDMahasiswaADO/DAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    internal class DAL
+    {
+        private SqlConnection conn = new SqlConnection(GetConnectionString());
+        private SqlDataAdapter da;
+        private DataTable dtMahasiswa;
+
+        public static string GetConnectionString()
+        {
+            // Gunakan connection string statis yang sudah terbukti berhasil
+            return "Data Source=LAPTOP-M4FI536S\\RAMDANIAA;Initial Catalog=DBAkademikADO;Integrated Security=True;";
+        }
+
+        // ==================== COUNT ====================
+        
+    }
+}
